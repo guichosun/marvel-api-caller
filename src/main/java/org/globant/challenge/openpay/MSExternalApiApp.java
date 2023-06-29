4aa61226fae038309e73f0d71f3cb444c38731d1
alberto.delcampo-at-676770050448 <luis.albertodelcampo@gmail.com>
2023-06-28 18:04:15 -0600

    I create project regarding call the Marvel API through a RestTemplate object

---
+++ src/main/java/org/globant/challenge/openpay/MSExternalApiApp.java
@@ -0,0 +1,17 @@
+package org.globant.challenge.openpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for the Spring-boot application,
+ *
+ * @author luis.delcampo
+ */
+@SpringBootApplication
+public class MSExternalApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSExternalApiApp.class, args);
+    }
+}
